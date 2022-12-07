2785600582d1a5bae73c53c12defcea9b8fb7cd1
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-07 22:49:01 +0200

    Made Makefile

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alex\n");
+
+        if (args.length != 1) {
+            System.err.println("Usage: Tema2 <workers> <in_file> <out_file>");
+            return;
+        }
+
+        int NUMBER_OF_THREADS = Integer.parseInt(args[0]);
+
+        System.out.println(NUMBER_OF_THREADS);
+
     }
+
+
+
 }
