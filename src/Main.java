d96fad87d9d1f3febb3ab0edebc04b86d31056cf
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-07 23:06:56 +0200

    Made the File Input System

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-        if (args.length != 1) {
-            System.err.println("Usage: Tema2 <workers> <in_file> <out_file>");
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        if (args.length != 2) {
+            System.err.println("Wrong number of arguments, to run it use the following format:\n");
+            System.out.println("java Main <input file> <number of threads>");
             return;
         }
 
-        int NUMBER_OF_THREADS = Integer.parseInt(args[0]);
+        File ordersInput = new File(args[0] + "/orders.txt");
+        File productsInput = new File(args[0] + "/order_products.txt");
+        int threadsNumber = Integer.parseInt(args[1]);
 
-        System.out.println(NUMBER_OF_THREADS);
+        Scanner ordersScanner = new Scanner(ordersInput);
+        Scanner productsScanner = new Scanner(productsInput);
+
+        
 
     }
-
-
-
 }
