d8015295fde3305382a5a096f8bb1801804136e2
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-08 12:25:27 +0200

    Created OutputFiles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
+class Constants {
+    static final String inputOrdersPath = "/orders.txt";
+    static final String inputProductsPath = "/order_products.txt";
+    static final String outputOrdersPath = "orders_out.txt";
+    static final String outputProductsPath = "order_products_out.txt";
+}
+
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         if (args.length != 2) {
             System.err.println("Wrong number of arguments, to run it use the following format:\n");
@@ -11,14 +19,27 @@ public class Main {
             return;
         }
 
-        File ordersInput = new File(args[0] + "/orders.txt");
-        File productsInput = new File(args[0] + "/order_products.txt");
+        File ordersInput = new File(args[0] + Constants.inputOrdersPath);
+        File productsInput = new File(args[0] + Constants.inputProductsPath);
         int threadsNumber = Integer.parseInt(args[1]);
 
         Scanner ordersScanner = new Scanner(ordersInput);
         Scanner productsScanner = new Scanner(productsInput);
 
-        
+        File ordersOutputFile = new File(Constants.outputOrdersPath);
+        File productsOutputFile = new File(Constants.outputProductsPath);
+
+        ordersOutputFile.createNewFile();
+        productsOutputFile.createNewFile();
+
+        FileWriter ordersWriter = new FileWriter(Constants.outputOrdersPath);
+        FileWriter productsWriter = new FileWriter(Constants.outputProductsPath);
+
+        ordersWriter.write("Alex");
+        productsWriter.write("Bogdan");
+
+        ordersWriter.close();
+        productsWriter.close();
 
     }
 }
